4fda3fca69cb1815bfafb6fcecb95a99c8c023dd
itmanharry <jack_chengqian@163.com>
2023-01-09 14:58:27 +0800

    hmac authenticate

--- cookbook/chapter11/sample9_auth.py
+++ cookbook/chapter11/sample9_auth.py
@@ -16,7 +16,7 @@ def client_authenticate(connection, secret_key):
     secret_key is a key known only to both client/server.
     '''
     message = connection.recv(32)
-    hash = hmac.new(secret_key, message)
+    hash = hmac.new(secret_key, message, digestmod='sha1')
     digest = hash.digest()
     connection.send(digest)
 def server_authenticate(connection, secret_key):
@@ -25,7 +25,7 @@ def server_authenticate(connection, secret_key):
     '''
     message = os.urandom(32)
     connection.send(message)
-    hash = hmac.new(secret_key, message)
+    hash = hmac.new(secret_key, message, digestmod='sha1')
     digest = hash.digest()
     response = connection.recv(len(digest))
     return hmac.compare_digest(digest,response)
